22facb5f1614833fce323dad35c613b153db9ddc
AlbertLanne <albert.lanne@gmail.com>
2020-02-08 15:46:25 +0100

    requete put fonctionnelle

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
-const func = require('functions')
+// const func = require('functions')
+const {success,error} = require('functions')
 
 const members = [
     {
@@ -23,12 +24,49 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend: true}));
 
-app.get('/api/v1/members/:id', (req,res)=> {res.json(func.success(members[(req.params.id)-1].name))})
+app.get('/api/v1/members/:id', (req,res)=> {
+    let index = getIndex(req.params.id);
+
+    if (typeof(index) == 'string'){
+        res.json(error(index))
+    }else{
+        res.json(success(members[index]))
+    }
+    res.json(success(members[(req.params.id)-1].name))
+});
+
+app.put('/api/v1/members/:id', (req,res)=> {
+    
+    let index = getIndex(req.params.id);
+
+    if (typeof(index) == 'string'){
+        res.json(error(index))
+    }else{
+
+        let same = false;
+
+        for (let i = 0; i < members.length; i++){
+            if (req.body.name == members[i].name && req.params.id != members[i].id){
+                same = true
+                break
+            }
+        }
+
+        if (same){
+            res.json(error('same name'))
+        }else{
+            members[index].name = req.body.name
+            res.json(success(true))
+        }
+
+    }
+})
+
 app.get('/api/v1/members', (req,res)=> {
     if (req.query.max != undefined){
-        res.json(func.success(members.slice(0,req.query.max)))
+        res.json(success(members.slice(0,req.query.max)))
     }else {
-        res.json(func.success(members))
+        res.json(success(members))
     }
 })
 
@@ -45,7 +83,7 @@ app.post('/api/v1/members',(req,res)=>{
         }
 
         if (sameName){
-            res.json(func.error('name already taken'))
+            res.json(error('name already taken'))
 
         }else {
             let member ={
@@ -53,28 +91,25 @@ app.post('/api/v1/members',(req,res)=>{
                 name: req.body.name
             }
             members.push(member)
-            res.json(func.success(member))
+            res.json(success(member))
         }
 
 
 
     } else {
-        res.join(func.error('no name value'))
+        res.join(error('no name value'))
     }
 })
 
 
 
 app.listen(8080, () => console.log('Started on port 8080'))
-// function success(result){
-//     return {
-//         status: 'success',
-//         result: result
-//     }
-// }
-// function error(message){
-//     return {
-//         status: 'message',
-//         result: error
-//     }
-// }
+
+function getIndex(id) {
+    for (let i = 0; i < members.length; i++){
+        if (members[i].id == id)
+            return i
+    }
+    return 'wrong id'
+
+}
