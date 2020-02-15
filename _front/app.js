014d4eff5c67f48c3811b3b902e7c21dad9d1e9e
AlbertLanne <albert.lanne@gmail.com>
2020-02-15 21:27:28 +0100

    update front end

--- _front/app.js
+++ _front/app.js
@@ -1,135 +1,109 @@
 // Modules
+require('babel-register')
 const bodyParser = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')('dev')
 const axios = require('axios')
-//Variables globales
+const twig = require('twig')
+
+// Variables globales
 const app = express()
 const port = 8081
-const twig = require('twig')
+const fetch = axios.create({
+    baseURL: 'http://localhost:8080/api/v1'
+});
 
 // Middlewares
 app.use(morgan)
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// const swaggerUi = require('swagger-ui-express')
-// const swaggerDocument = require('./assets/swagger.json')
-
-const config = require('../assets/classes/config')
-const fetch = axios.create({
-    baseURL : 'http://localhost:8080/api/v1'
-})
-
-
+// Routes
 
 // Page d'accueil
 app.get('/', (req, res) => {
     res.redirect('/members')
 })
 
-
-
-app.get('/members', (req,res)=>{
-    fetch.get('/members')
-            .then((response)=>{
-
-                if(response.data.status == 'success') {
-                    res.render('members.twig',{
-                        members : response.data.result})
-                } else {
-                    renderError(res, response.data.message)
-                }
+// Page récupérant tous les membres
+app.get('/members', (req, res) => {
+    apiCall(req.query.max ? '/members?max='+req.query.max : '/members', 'get', {}, res, (result) => {
+        res.render('members.twig', {
+            members: result
         })
-            .catch((err)=>renderError(res,err.message))
+    })
+})
 
+// Page récupérant un membre en fonction de son ID
+app.get('/members/:id', (req, res) => {
+    apiCall('/members/'+req.params.id, 'get', {}, res, (result) => {
+        res.render('member.twig', {
+            member: result
+        })
+    })
+})
+
+
+// Page gérant la modification d'un membre
+app.get('/edit/:id', (req, res) => {
+    apiCall('/members/'+req.params.id, 'get', {}, res, (result) => {
+        res.render('edit.twig', {
+            member: result
+        })
+    })
+})
+
+// Méthode permettant de modifier un membre
+app.post('/edit/:id', (req, res) => {
+    apiCall('/members/'+req.params.id, 'put', {
+        name: req.body.name
+    }, res, () => {
+        res.redirect('/members')
+    })
+})
+
+// Méthode permettant de supprimer un membre
+app.post('/delete', (req, res) => {
+    apiCall('/members/'+req.body.id, 'delete', {}, res, () => {
+        res.redirect('/members')
+    })
+})
+
+// Page gérant l'ajout d'un membre
+app.get('/insert', (req, res) => {
+    res.render('insert.twig')
+})
+
+// Méthode permettant d'ajouter un membre
+app.post('/insert', (req, res) => {
+    apiCall('/members', 'post', {name: req.body.name}, res, () => {
+        res.redirect('/members')
+    })
 })
 
 
 // Lancement de l'application
-app.listen(port, ()=>  console.log('started on port ' + port))
-
+app.listen(port, () => console.log('Started on port ' + port))
 
 // Fonctions
 
-function renderError(res, errMsg){
-    res.render('error.twig',{
-        errorMsg : errMsg
+function renderError(res, errMsg) {
+    res.render('error.twig', {
+        errorMsg: errMsg
     })
 }
 
-
-
-
-
-
-
-
-
-// mysql.createConnection({
-//     host: config.db.host,
-//     database: config.db.database,
-//     user: config.db.user,
-//     password: config.db.password
-//
-//
-// }).then((db)=> {
-//
-//     console.log('Connected.')
-//
-//     const app = express()
-//
-//     let MembersRouter = express.Router()
-//     let Members = require('./assets/classes/members-class')(db, config)
-//     console.log(Members)
-//
-//
-//
-//     // SWAGGERS  http://localhost:8080/api/v1/api-docs/#/
-//     app.use(config.rootAPI + 'api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-//
-//
-//     MembersRouter.route('/:id')
-//
-//         // Récupère un membre avec son ID
-//
-//         .get(async (req, res) => {
-//             let member = await Members.getById(req.params.id)
-//             res.json(checkAndchange(member))
-//         })
-//
-//         // Modifie un membre avec ID
-//         .put(async(req, res) => {
-//             let updateMember = await Members.update(req.params.id, req.body.name)
-//             res.json(checkAndchange(updateMember))
-//         })
-//
-//         // Supprime un membre avec ID
-//         .delete(async (req, res) => {
-//             let deleteMember = await Members.delete(req.params.id)
-//             res.json(checkAndchange(deleteMember))
-//         })
-//
-//     MembersRouter.route('/')
-//
-//         // Récupère tous les membres
-//         .get(async (req, res) => {
-//            let allMembers = await Members.getAll(req.query.max)
-//             res.json(checkAndchange((allMembers)))
-//         })
-//
-//         // Ajoute un membre avec son nom
-//         .post(async (req, res) => {
-//             let addMember = await Members.add(req.body.name)
-//             res.json(checkAndchange(addMember))
-//         })
-//
-//     app.use(config.rootAPI+'members', MembersRouter)
-//
-//
-//
-//
-// }).catch((err)=>{
-//     console.log('Error During database connection')
-//     console.log(err.message)
-// })
+function apiCall(url, method, data, res, next) {
+    fetch({
+        method: method,
+        url: url,
+        data: data
+    }).then((response) => {
+        if (response.data.status == 'success') {
+            next(response.data.result)
+        } else {
+            renderError(res, response.data.message)
+        }
+    })
+        .catch((err) => renderError(res, err.message))
+}
